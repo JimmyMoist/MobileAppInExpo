0fe98c4ca7e96243b1973c3fb59cbeca25ddf806
JimmyMoist <115209086+JimmyMoist@users.noreply.github.com>
2025-03-03 23:03:46 -0800

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,4 +1,5 @@
-import { Button, View, Text } from "react-native";
+import React from "react";
+import { View, Button, Text, StyleSheet } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -8,3 +9,8 @@ const HomeScreen = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 20, fontWeight: "bold", marginBottom: 20 },
+});
